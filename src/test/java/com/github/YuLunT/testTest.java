ba06cfe178a4a51c5777fa23dad98fec4a99a75e
YuLunT <4407123@gmail.com>
2017-12-01 20:22:52 +0800

    test

---
+++ src/test/java/com/github/YuLunT/testTest.java
@@ -0,0 +1,18 @@
+package com.github.YuLunT;
+
+import static org.junit.Assert.*;
+
+public class testTest {
+    @org.junit.Test
+    public void main() throws Exception {
+
+        Triangle test = new Triangle(a:5,b:10,c:5);
+
+
+        String result = triangle.getType();
+
+
+        Assert.assertEquals( expected: "不是三角形",result);
+    }
+
+}
